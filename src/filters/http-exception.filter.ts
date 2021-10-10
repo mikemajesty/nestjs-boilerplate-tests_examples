f136ebf599e034901359c9ea2c90a14b89dc931b
Mike Lima <mike.lima.ext@dasa.com.br>
2021-10-10 17:50:01 -0300

    add lint

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -3,7 +3,7 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
-  HttpStatus
+  HttpStatus,
 } from '@nestjs/common';
 import * as moment from 'moment';
 import { v4 as uuidv4 } from 'uuid';
