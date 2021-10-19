25d47d16773b22e6d97e36ecae06c54c04bea09f
Mike Lima <mike.lima.ext@dasa.com.br>
2021-10-19 20:11:56 -0300

    style(http-exception.filter): add comma

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -3,7 +3,7 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
-  HttpStatus
+  HttpStatus,
 } from '@nestjs/common';
 import * as moment from 'moment';
 import { LoggerService } from '../modules/global/logger/service';
