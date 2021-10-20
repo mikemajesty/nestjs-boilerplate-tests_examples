46f6e11581ce45500a4f2df046007195b7e062a1
Mike Lima <mike.lima.ext@dasa.com.br>
2021-10-20 15:42:52 -0300

    refactor(secretservice): extents ConfigService

--- src/modules/global/secrets/service.ts
+++ src/modules/global/secrets/service.ts
@@ -9,14 +9,14 @@ export enum Variables {
 }
 
 @Injectable()
-export class SecretsService implements ISecretsService {
-  private configService: ConfigService = new ConfigService();
+export class SecretsService extends ConfigService implements ISecretsService {
+  constructor() {
+    super();
+  }
 
-  ENV = this.configService.get<string>(Variables.ENV);
-  PORT = this.configService.get<number>(Variables.PORT) || 3000;
+  ENV = this.get<string>(Variables.ENV);
+  PORT = this.get<number>(Variables.PORT) || 3000;
   url = {
-    HELLO_WORD_SERVICE: this.configService.get<string>(
-      Variables.HELLO_WORD_SERVICE,
-    ),
+    HELLO_WORD_SERVICE: this.get<string>(Variables.HELLO_WORD_SERVICE),
   };
 }
